8fcc1fb958ccd5b3632e61eed6ccb35d507ad5ef
Kukovec <jure.kukovec@gmail.com>
2026-08-21 09:54:16 +0200

    rm import

--- Dalek32/Lint/SpecIndent.lean
+++ Dalek32/Lint/SpecIndent.lean
@@ -3,7 +3,6 @@ Copyright (c) 2026 The Beneficial AI Foundation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Liao Zhang
 -/
-import Lean
 import Mathlib.Tactic.Linter
 
 /-!
