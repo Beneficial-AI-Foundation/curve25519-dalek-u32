24a91d66ea183c728aeb3832c02582fced26fa26
András Némedy Varga <nemedyva@gmail.com>
2026-08-30 11:48:20 +0200

    Updating Dalek32.lean

--- Dalek32.lean
+++ Dalek32.lean
@@ -1,6 +1,7 @@
 import Dalek32.Field.FromLimbs
 import Dalek32.Field.from_bytes.LOW23BITS
 import Dalek32.Field.reduce.LOW25BITS
+import Dalek32.Field.reduce.LOW26BITS
 import Dalek32.Field.to_bytes.LOW25BITS
 import Dalek32.Field.to_bytes.LOW26BITS
 import Dalek32.Lint.Basic
