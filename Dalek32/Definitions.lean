1149d1e5b6a0e52cf038a4121106580a6abc0864
András Némedy Varga <nemedyva@gmail.com>
2026-09-02 19:47:15 +0200

    Fixing doc-string and adding explicit return type

--- Dalek32/Definitions.lean
+++ Dalek32/Definitions.lean
@@ -22,8 +22,8 @@ def Array.uScalarToNatRadix {ty : UScalarTy} {n : Usize}
   (limbs : Array (UScalar ty) n) (exp : Nat := ty.numBits) : Nat :=
   ∑ i ∈ Finset.range n.val, 2 ^ (exp * i) * (limbs[i]!).val
 
-/-- Turns an `Array (UScalar ty) Usize` into a `Nat` using `2 ^ exp` as the radix.
-Omitting `exp` the radix defaults to `2^ty.numBits`. -/
+/-- Turns an `Array (UScalar ty) 10#usize` into a `Nat` using `2 ^ 26` and `2 ^ 25` alternatingly
+as a radix. -/
 def Array.uScalarToNatField2625 {ty : UScalarTy} (limbs : Array (UScalar ty) 10#usize) : Nat :=
   ∑ i ∈ Finset.range 10, 2 ^ (26 * ((i + 1) / 2) + 25 * (i / 2)) * (limbs[i]!).val
 
@@ -36,7 +36,7 @@ open Aeneas Aeneas.Std
 
 namespace curve25519_dalek
 
-/-- The field prime `p = 2^255 - 19` -/
+/-- The field prime `p = 2^255 - 19`. -/
 def p : Nat := 2 ^ 255 - 19
 
 attribute [irreducible] p
@@ -46,7 +46,9 @@ attribute [irreducible] p
 
 namespace backend.serial.u32.field.FieldElement2625
 
-def toNat (x : FieldElement2625) := Array.uScalarToNatField2625 x
+/-- Interpret a `FieldElement2625` (ten u32 limbs used to represent 26/25 bits alternatingly) as a
+natural number. -/
+def toNat (x : FieldElement2625) : Nat := Array.uScalarToNatField2625 x
 
 end backend.serial.u32.field.FieldElement2625
 
